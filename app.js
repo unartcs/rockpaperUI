e43ae3cc0fb91c522a8480e53579def103402e4a
unartcs <unartcs@gmail.com>
2022-08-25 01:19:10 +0200

    Edited all files, added images
    Basically completed(?) the project
    Added start, restart buttons and added images (had to change the method of getting the playerChoice to data instead of class (e.path[0].attributes[0].nodeValue)

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-/* RPS game - player has 3 choices, chooses one > the computer chooses randomly
-and then we compare the winner gets a point and next round starts*/
-const WEAPONS = ['Rock','Paper','Scissors'];
+const WEAPONS = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
 let maxRounds = 5;
 let showScore = `Player score: ${playerScore} Computer score: ${computerScore}`;
+const gameText = document.querySelector('.game-text');
+const playerScoreHtml = document.querySelector('.player-score');
+const computerScoreHtml = document.querySelector('.computer-score');
+const restartButton = document.querySelector('.restart-button');
+const startButton = document.querySelector('.start-button')
+const imageContainer = document.querySelector('.image-container');
 
-// Rock = 1, Paper = 2, Scissors = 3.
+
 function startGame() {
-    startRound();
+    startButton.classList.remove('hide');
+    startButton.addEventListener('click', function x() {
+        imageContainer.classList.remove('hide');
+        startButton.classList.add('hide');
+        startButton.removeEventListener('click', x);
+        startRound();
+    })
 }
 
 function startRound() {
-    const button = document.querySelectorAll('.player-buttons button')
+    const button = document.querySelectorAll(".image-content")
     let roundPlayed = false;
+    gameText.style.color = 'black';
     button.forEach((button) => {
-        button.addEventListener('click', (e) => {
+        button.addEventListener('click', (e) => {  
             if (roundPlayed != false) return;
-            playerChoice = button.classList.value;
+            playerChoice = (e.path[0].attributes[0].nodeValue)
+            //playerChoice = button.classList.value; - Old method of getting the choice
+            //Changed to the above method in order to be able to press the image+text+image container itself to choose the weapon
             playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
             roundPlayed = true;
-            //console.log(playerChoice);
             getComputerChoice(playerChoice);
         })
     }
@@ -28,8 +40,9 @@ function startRound() {
 }
 
 function getComputerChoice(playerChoice) {
-    ComputerChoice = Math.floor(Math.random() * WEAPONS.length+1);
-    switch(ComputerChoice) {
+    let ComputerChoice = Math.floor(Math.random() * WEAPONS.length + 1);
+    let computerChoiceStr;
+    switch (ComputerChoice) {
         case 1:
             computerChoiceStr = 'Rock';
             break;
@@ -41,36 +54,63 @@ function getComputerChoice(playerChoice) {
             break;
     }
     if (computerChoiceStr == 'Rock' && playerChoice == 'Paper' || computerChoiceStr == 'Paper' && playerChoice == 'Scissors' || computerChoiceStr == 'Scissors' && playerChoice == 'Rock') {
-        playerWin(computerChoiceStr,playerChoice);
+        playerWin(computerChoiceStr, playerChoice);
     } else if (playerChoice == 'Rock' && computerChoiceStr == 'Paper' || playerChoice == 'Paper' && computerChoiceStr == 'Scissors' || playerChoice == 'Scissors' && computerChoiceStr == 'Rock') {
-        computerWin(computerChoiceStr,playerChoice);
+        computerWin(computerChoiceStr, playerChoice);
     } else {
+        gameText.style.backgroundColor = 'yellow';
+        gameText.innerHTML = 'Its a tie!'
         console.log('Its a tie!')
-        startRound(); 
+        startRound();
     }
 }
 
 function playerWin(computerChoice, playerChoice) {
-    playerScore++;
+    playerScore = playerScore + 1;
     console.log(`You won the round! Computer chose ${computerChoice}! ${showScore}`)
-    playerScoreHtml = document.querySelector('.player-score')
+    gameText.style.backgroundColor = 'green';
+    gameText.innerHTML = (`You won the round, Computer chose ${computerChoice}!`);
     playerScoreHtml.innerHTML = `Player Score: ${playerScore}`;
     if (playerScore == 5) {
         console.log(`Game over, you won! ${showScore}`)
+        gameText.innerHTML = "You Won!"
+        restartGame();
     }
-    else startGame();
+    else startRound();
 }
 
 function computerWin(computerChoice, playerChoice) {
-    computerScore++;
-    console.log(computerScore)
+    computerScore = computerScore + 1;
     console.log(`You lost the round! Computer chose ${computerChoice}! ${showScore}`)
-    computerScoreHtml = document.querySelector('.computer-score')
+    gameText.style.backgroundColor = 'red';
+    gameText.innerHTML = (`You lost the round, Computer chose ${computerChoice}!`);
     computerScoreHtml.innerHTML = `Computer Score: ${computerScore}`
     if (computerScore == 5) {
         console.log(`Game over, you lost! ${showScore}`)
+        gameText.innerHTML = "You Lost!"
+        restartGame();
     }
-    else startGame();
+    else startRound();
 }
+
+function restartGame() {
+    //When the game ends there will be a "Restart game pop" - maybe add a class to a hidden element?
+    imageContainer.classList.add('hide')
+    restartButton.classList.add('show');
+    restartButton.addEventListener("click", function a () {
+        restartButton.classList.remove('show');
+        computerScore = 0;
+        playerScore = 0;
+        gameText.innerHTML = '';
+        playerScoreHtml.innerHTML = `Player Score: 0`;
+        computerScoreHtml.innerHTML = `Computer Score: 0`;
+        console.log(computerScore)
+        console.log(playerScore)
+        restartButton.removeEventListener("click",a);
+        startGame();
+    });
+    
+}
+
 
 startGame();
